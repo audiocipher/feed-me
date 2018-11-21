0f0a832d82115d6ececa61f1c17de0eb182dbc28
AJ McClure <allen_mcclure@homedepot.com>
2018-11-20 19:13:09 -0600

    added sample material-ui components

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,13 @@ import React, { Component } from 'react';
 import './App.css';
 import FoodPicker from './components/FoodPicker';
 
+import ButtonAppBar from './components/ButtonAppBar';
+
 class App extends Component {
   render() {
     return (
       <div>
+        <ButtonAppBar />
         <h1>Feed Me</h1>
         <FoodPicker />
       </div>
